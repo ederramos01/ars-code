768d843d2d0147abcba5715e9b3ddca0adc68047
ederramos01 <eder.ramos01@telusinternational.com>
2023-05-22 10:40:47 -0600

    backup of code

---
+++ ars-code-bck.js
@@ -0,0 +1,80 @@
+//This is the ARS Engine [month] link
+var sourceSheetLink = 'https://docs.google.com/spreadsheets/d/1otYwseHbr6DYcC544-S0AlZjKVo76jsXMOFJXs5LbSA/edit#gid=647400929';
+
+function getDataForARSFAHW() {
+    importData();
+    getScheduleSV();
+    getDistributionSV();
+}
+
+function importData() {
+    removeDataAndSchedules('RD', 13);
+    var sourceSheet = SpreadsheetApp.openByUrl(sourceSheetLink);
+    var sourceTab = sourceSheet.getSheetByName('CMR Hours');
+    var commandTab = sourceSheet.getSheetByName('Command tab');
+    var rows = commandTab.getRange('C13').getValue();
+    var data = sourceTab.getRange(1, 1, rows, 13).getDisplayValues();
+    var thiSheet = SpreadsheetApp.getActiveSpreadsheet()
+    var desSheet = thiSheet.getSheetByName('RD')
+    var rango = desSheet.getRange(1, 1, rows, 13);
+    rango.setValues(data)
+}
+
+function getScheduleSV() {
+    //clearScheduleSV();
+    removeDataAndSchedules('Schedules SV', 39);
+    var sourceSheet = SpreadsheetApp.openByUrl(sourceSheetLink);
+    var sourceTab = sourceSheet.getSheetByName('Schedules SV');
+    var commandTab = sourceSheet.getSheetByName('Command tab');
+    var rows = commandTab.getRange('C14').getValue();
+    var data = sourceTab.getRange(1, 1, rows, 39).getDisplayValues();
+    var thiSheet = SpreadsheetApp.getActiveSpreadsheet();
+    var desSheet = thiSheet.getSheetByName('Schedules SV');
+    var rango = desSheet.getRange(1, 1, rows, 39);
+    rango.setValues(data)
+}
+
+
+function getDistributionSV() {
+    // clearDistributionSV();
+    removeDistribution('Schedules SV', 42, 33)
+    var sourceSheet = SpreadsheetApp.openByUrl(sourceSheetLink);
+    var sourceTab = sourceSheet.getSheetByName('Schedules SV');
+    var commandTab = sourceSheet.getSheetByName('Command tab');
+    var rows = commandTab.getRange('C14').getValue();
+    var data = sourceTab.getRange(1, 117, rows, 33).getDisplayValues();
+    var thiSheet = SpreadsheetApp.getActiveSpreadsheet();
+    var desSheet = thiSheet.getSheetByName('Schedules SV');
+    var rango = desSheet.getRange(1, 42, rows, 33);
+    rango.setValues(data)
+}
+
+function removeDataAndSchedules(sheetName, numberColumns) {
+    var thiSheet = SpreadsheetApp.getActiveSpreadsheet();
+    var desTab = thiSheet.getSheetByName(sheetName);
+    desTab.getRange(1, 1, desTab.getMaxRows(), numberColumns).clearContent();
+}
+
+function removeDistribution(sheetName, columnPosition, numberColumns) {
+    var thiSheet = SpreadsheetApp.getActiveSpreadsheet();
+    var desTab = thiSheet.getSheetByName(sheetName);
+    desTab.getRange(1, columnPosition, desTab.getMaxRows(), numberColumns).clearContent();
+}
+
+// function clearData() {
+//   var thiSheet = SpreadsheetApp.getActiveSpreadsheet();
+//   var desTab = thiSheet.getSheetByName('RD');
+//   desTab.getRange(1, 1, desTab.getMaxRows(), 13).clear();
+// }
+
+// function clearScheduleSV() {
+//   var thiSheet = SpreadsheetApp.getActiveSpreadsheet();
+//   var desTab = thiSheet.getSheetByName('Schedules SV');
+//   desTab.getRange(1, 1, desTab.getMaxRows(), 39).clear();
+// }
+
+// function clearDistributionSV() {
+//   var thiSheet = SpreadsheetApp.getActiveSpreadsheet();
+//   var desTab = thiSheet.getSheetByName('Schedules SV');
+//   desTab.getRange(1, 42, desTab.getMaxRows(), 33).clear();
+// }
---
+++ ars-code.js
@@ -0,0 +1,62 @@
+//This is the ARS Engine [month] link
+var sourceSheetLink = 'https://docs.google.com/spreadsheets/d/1otYwseHbr6DYcC544-S0AlZjKVo76jsXMOFJXs5LbSA/edit#gid=647400929';
+
+function getDataForARSFAHW() {
+    importData();
+    getSchedule('Schedules SV', 39);
+    getDistribution('Schedules SV', 42, 33);
+    getSchedule('Schedules GT', 39);
+    getDistribution('Schedules GT', 42, 33);
+}
+
+function importData() {
+    removeDataAndSchedules('RD', 16);
+    var sourceSheet = SpreadsheetApp.openByUrl(sourceSheetLink);
+    var sourceTab = sourceSheet.getSheetByName('CMR Hours');
+    var commandTab = sourceSheet.getSheetByName('Command tab');
+    var rows = commandTab.getRange('C13').getValue();
+    var data = sourceTab.getRange(1, 1, rows, 16).getDisplayValues();
+    var thiSheet = SpreadsheetApp.getActiveSpreadsheet()
+    var desSheet = thiSheet.getSheetByName('RD')
+    var rango = desSheet.getRange(1, 1, rows, 16);
+    rango.setValues(data)
+}
+
+function getSchedule(tabName, columnRange) {
+    removeDataAndSchedules(tabName, columnRange);
+    var sourceSheet = SpreadsheetApp.openByUrl(sourceSheetLink);
+    var sourceTab = sourceSheet.getSheetByName(tabName);
+    var commandTab = sourceSheet.getSheetByName('Command tab');
+    var rows = commandTab.getRange('C14').getValue();
+    var data = sourceTab.getRange(1, 1, rows, columnRange).getDisplayValues();
+    var thiSheet = SpreadsheetApp.getActiveSpreadsheet();
+    var desSheet = thiSheet.getSheetByName(tabName);
+    var rango = desSheet.getRange(1, 1, rows, columnRange);
+    rango.setValues(data)
+}
+
+
+function getDistribution(tabName, columnPosition, numberColumns) {
+    removeDistribution(tabName, columnPosition, numberColumns)
+    var sourceSheet = SpreadsheetApp.openByUrl(sourceSheetLink);
+    var sourceTab = sourceSheet.getSheetByName(tabName);
+    var commandTab = sourceSheet.getSheetByName('Command tab');
+    var rows = commandTab.getRange('C14').getValue();
+    var data = sourceTab.getRange(1, 117, rows, numberColumns).getDisplayValues();
+    var thiSheet = SpreadsheetApp.getActiveSpreadsheet();
+    var desSheet = thiSheet.getSheetByName(tabName);
+    var rango = desSheet.getRange(1, columnPosition, rows, numberColumns);
+    rango.setValues(data)
+}
+
+function removeDataAndSchedules(sheetName, numberColumns) {
+    var thiSheet = SpreadsheetApp.getActiveSpreadsheet();
+    var desTab = thiSheet.getSheetByName(sheetName);
+    desTab.getRange(1, 1, desTab.getMaxRows(), numberColumns).clearContent();
+}
+
+function removeDistribution(sheetName, columnPosition, numberColumns) {
+    var thiSheet = SpreadsheetApp.getActiveSpreadsheet();
+    var desTab = thiSheet.getSheetByName(sheetName);
+    desTab.getRange(1, columnPosition, desTab.getMaxRows(), numberColumns).clearContent();
+}
